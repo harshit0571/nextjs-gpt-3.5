12a896c0c12431d2dd2c1d70cb3bd517aae59bfc
harshit0571 <harshitchadha65@gmail.com>
2023-06-17 23:24:36 +0530

    openai set

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const send = (prompt) => {};
   return (
     <main>
       <div>helo world</div>
