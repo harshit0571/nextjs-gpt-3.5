aab31d014ca3ec1cadce9aef122ebd9c802b63fb
harshit0571 <harshitchadha65@gmail.com>
2023-06-18 18:41:19 +0530

    added gpt3.5 turboaxios for api client side return function used in api/openai function instead of res.status createChatCompletion instead of createCompletion

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,54 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { useEffect, useState } from "react";
+import axios, { Axios } from "axios";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const send = (prompt) => {};
+  const [Response, setResponse] = useState("");
+  const [IsLoading, setIsLoading] = useState("");
+  // const send = async (p) => {
+  //   setResponse("");
+  //   console.log("Getting response from OpenAI...");
+  //   setIsLoading(true);
+  //   const response = await fetch("/api/openai", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({ prompt: p }),
+  //   });
+  //   const data = await response.json();
+  //   setIsLoading(false);
+  //   setResponse(data.text);
+  // };
+
+  async function getGPT(p) {
+    const client = axios.create({
+      headers: { "Content-Type": "application/json" },
+    });
+    const params = {
+      prompt: p,
+    };
+    client
+      .post("api/openai", JSON.stringify(params))
+      .then((result) => {
+        setResponse(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  // send("what is 2+2");
+  useEffect(() => {
+    getGPT("write a 200 words essay on airpolution");
+  }, []);
+
   return (
     <main>
-      <div>helo world</div>
+      <div>{Response.data.text}</div>
     </main>
   );
 }
--- utils/openai.js
+++
@@ -1,9 +0,0 @@
-import { Configuration, OpenAIApi } from "openai";
-const configuration = new Configuration({
-  organization: "org-PyidNvhpRCtOuJTmXI9bkNOe",
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-const response = await openai.listEngines();
-
-export default openai;
